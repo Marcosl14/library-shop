5f6e9559d7c2b1aa00f252ad6b81e0a7c1df8338
Marcos Giordano <marcos_l14@hotmail.com>
2022-02-20 20:12:19 -0300

    Create user entity

---
+++ src/users/models/user.entity.ts
@@ -0,0 +1,82 @@
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @ApiProperty({
+    description: 'User id number',
+    type: Number,
+  })
+  @PrimaryGeneratedColumn({
+    unsigned: true,
+    type: 'bigint',
+  })
+  id: number;
+
+  @ApiHideProperty()
+  @Column({
+    name: 'password',
+    type: 'character varying',
+    length: 100,
+    nullable: false,
+    select: false,
+  })
+  password: string;
+
+  @ApiProperty({
+    description: 'Email valid of user ',
+    type: String,
+  })
+  @Column({
+    name: 'email',
+    type: 'character varying',
+    length: 100,
+    nullable: false,
+  })
+  email: string;
+
+  @ApiHideProperty()
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: null,
+    select: false,
+  })
+  createdAt?: Date;
+
+  @ApiHideProperty()
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: null,
+    select: false,
+  })
+  updatedAt?: Date;
+
+  @ApiHideProperty()
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    type: 'timestamptz',
+    default: null,
+    select: false,
+  })
+  deletedAt?: Date;
+
+  //   @ApiHideProperty()
+  //   @Column({
+  //     type: 'integer',
+  //     nullable: false,
+  //     name: 'account_status',
+  //     default: AccountStatusesEnum.PENDING,
+  //     enum: AccountStatusesEnum,
+  //     select: false,
+  //   })
+  //   account_status: AccountStatusesEnum;
+}
