52284a8fa482cb7251581e5b6c7f0154fc0750c6
Marcos Giordano <marcos_l14@hotmail.com>
2022-03-06 10:16:42 -0300

    Add throttler guard to login and register endpoints

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -5,12 +5,14 @@ import { UserLoginDTO } from 'src/users/models/user-login.dto';
 import { Public } from '../decorators/public.decorator';
 import { AuthService } from '../services/auth.service';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
+  @UseGuards(ThrottlerGuard)
   @Post('register')
   async register(@Body() userDTO: UserRegistryDTO) {
     await this.authService.userExists(userDTO);
@@ -22,6 +24,7 @@ export class AuthController {
 
   @Public()
   @UseGuards(LocalAuthGuard)
+  @UseGuards(ThrottlerGuard)
   @Post('login')
   async login(@Body() userDTO: UserLoginDTO, @Req() req) {
     await this.authService.validatePassword(userDTO.password, req.user);
