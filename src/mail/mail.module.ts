4c68c3eec9d8be46c13e603f0c0ca857e2802001
Marcos Giordano <marcos_l14@hotmail.com>
2022-03-06 18:07:49 -0300

    Configure mailer

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,36 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailService } from './services/mail.service';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          service: 'gmail',
+          auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASSWORD,
+          },
+        },
+        defaults: {
+          from: `No responder <${process.env.MAIL_USER}>`,
+        },
+        template: {
+          dir: join(__dirname, '/templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
