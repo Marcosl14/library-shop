8308072f165d656e103d15f6eff491429d69dc17
Marcos Giordano <marcos_l14@hotmail.com>
2022-03-06 18:08:26 -0300

    Create raw constants to fill mailer data

---
+++ src/mail/constants/constants.ts
@@ -0,0 +1,10 @@
+import { Encoding } from 'crypto';
+
+export const cryptoConstants = {
+  ALGORITHM: 'aes-256-cbc',
+  KEY: Buffer.alloc(32).fill('jaksfn jafkn jafnsafskajlwqjr'),
+  INITIAL_VECTOR: Buffer.alloc(16).fill('jaksfn jafkn jafnsafskajlwqjr'),
+  INPUT_ENCODING: 'utf8' as Encoding,
+  OUTPUT_ENCODING: 'hex' as Encoding,
+  VALIDITY_TIME: 1000 * 60 * 60 * 24,
+};
