8a77bc9551f45015fbd8b6fc1eff78c0efa4904c
Marcos Giordano <marcos_l14@hotmail.com>
2022-03-20 09:43:07 -0300

    Uuid verification in DTO not in controller

--- src/auth/models/confirm-registration.dto.ts
+++ src/auth/models/confirm-registration.dto.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class ConfirmRegistrationDTO {
   @ApiProperty({
     description: 'Registration Identifier',
     type: String,
+  })
+  @IsUUID('all', {
+    message: 'VALUE_IS_NOT_UUID',
   })
   @IsNotEmpty({
     message: 'EMPTY_REGISTRATION_IDENTIFIER',
